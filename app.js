44015d766a763ac6a9377ca35f6279c647a14fd8
Good-ho <whdxo100@gmail.com>
2020-10-14 22:21:24 +0900

    Context
    	1. Canvas mdn 참고.
    	- getContext를 통해 canvas context를 가져옴.
    	- Context에 line, stroke 등 설정.
    	- Pating 중(moveMove)이면 lineTo -> stroke로 그린다.
    	
    	2. 아래 코드 분석
    	- 클릭이 되기 전까지 canvas위에 있는 마우스 path를 추적한다.
    	- 그리고 있지만 않지 위치는 계속 알고 있는 것.
    	- 클릭이 되면 이제 그리기 시작하는 것.
    	- 즉, 클릭전까지 위치 추적하고 있고, 클릭이 되면 아주 작은 선들이 연결되어 눈에 보이는 선이되는 것.
    	- Mdn에 lineTo 내용을 보면 아래와 같이 기술되어 있음.
    
    	function onMouseMove(event){
    	    const x = event.offsetX;
    	    const y = event.offsetY;    
    	    if(!isPaint){
    	        ctx.beginPath();
    	        ctx.moveTo(x, y);
    	    } else {
    	        ctx.lineTo(x, y);
    	        ctx.stroke();
    	    }
    }

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const canvas = document.getElementById("jsCanvas");
 
+canvas.width = 700;
+canvas.height = 600;
+
 let isPaint = false;
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = "#2c2c2c"
+ctx.lineWidth = 2.5;
+
+function startPainting(){
+    isPaint = true;
+}
+
+function stopPainting(event){
+    isPaint = false;
+}
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;    
-}
-
-function onMouseDown(event){
-    isPaint = true;
-}
-
-function onMouseUp(event){
-    isPaint = false;
-}
-
-function onMouseLeave(event){
-    isPaint = false;
+    if(!isPaint){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave", onMouseLeave);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
